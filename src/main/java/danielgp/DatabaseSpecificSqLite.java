f299dfe41c35c8acee4897c22d13f02840f7e838
Daniel Popiniuc <danielpopiniuc@gmail.com>
2025-04-18 02:42:32 +0300

    custom function to support Regular Expression search in SQLite was added

--- src/main/java/danielgp/DatabaseSpecificSqLite.java
+++ src/main/java/danielgp/DatabaseSpecificSqLite.java
@@ -3,6 +3,12 @@ package danielgp;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+/* SQLite function class */
+import org.sqlite.Function;
+/* Util classes */
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 
 /**
  * SQLite methods
@@ -23,6 +29,16 @@ public class DatabaseSpecificSqLite extends DatabaseResultSetingClass {
             connection = DriverManager.getConnection("jdbc:sqlite:" + strSqLiteFile);
             strFeedback = String.format("SQLite connection to %s database was successfully established!", strSqLiteFile);
             LogHandlingClass.LOGGER.debug(strFeedback);
+            Function.create(connection, "REGEXP_LIKE", new Function() {
+                @Override
+                protected void xFunc() throws SQLException {
+                    final String text = value_text(0);
+                    final String pattern = value_text(1);
+                    final Pattern regex = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+                    final Matcher matcher = regex.matcher(text);
+                    result(matcher.find() ? 1 : 0);
+                }
+            });
         } catch(SQLException e) {
             strFeedback = String.format("Connection failed: %s", e.getLocalizedMessage());
             LogHandlingClass.LOGGER.error(strFeedback);
