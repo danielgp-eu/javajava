962325f739530ea81772e8a6e96fce9c2887c6ea
Daniel Popiniuc <danielpopiniuc@gmail.com>
2025-11-13 23:50:37 +0200

    re-integration followin an operational test + few duplication code lines standardized

--- src/main/java/javajava/Example.java
+++ src/main/java/javajava/Example.java
@@ -4,6 +4,10 @@ package javajava;
 import org.apache.logging.log4j.Level;
 import picocli.CommandLine;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -14,9 +18,11 @@ import java.util.*;
     name = "top",
     subcommands = {
         GetInformationFromDatabase.class,
+        GetInformationFromTextWithSellingPointReceiptsIntoCsvFile.class,
         GetSpecificInformationFromSnowflake.class,
         GetSubFoldersFromFolder.class,
-        LogEnvironmentDetails.class
+        LogEnvironmentDetails.class,
+        PairBankRecordsWithSellingReceipts.class
     }
 )
 public final class Example implements Runnable {
@@ -57,6 +63,33 @@ public final class Example implements Runnable {
     @Override
     public void run() {
         // intentionally left commented
+    }
+}
+
+/**
+ * Gets information from Text for Selling Point Sale Into CSV file
+ */
+@CommandLine.Command(name = "GetInformationFromTextWithSellingPointReceiptsIntoCsvFile", description = "Gets information from text into CSV file")
+class GetInformationFromTextWithSellingPointReceiptsIntoCsvFile implements Runnable {
+
+    /**
+     * String for FileName
+     */
+    @CommandLine.Option(
+            names = {"-flIn", "--inFileName"},
+            description = "File Name to be analyzed",
+            arity = "1",
+            required = true)
+    private String strFileNameIn;
+
+    @Override
+    public void run() {
+        final String strFileNameOut = strFileNameIn + ".csv";
+        if (LoggerLevelProvider.currentLevel.isLessSpecificThan(Level.INFO)) {
+            final String strFeedback = String.format("I will process %s file and write results into %s as a CSV file", strFileNameIn, strFileNameOut);
+            LoggerLevelProvider.LOGGER.debug(strFeedback);
+        }
+        FileContentClass.getFileContentAsSellingPointReceiptIntoCsvFile(strFileNameIn, "MASTER TASTE", strFileNameOut);
     }
 }
 
@@ -258,5 +291,193 @@ class LogEnvironmentDetails implements Runnable {
     public void run() {
         final String strFeedback = EnvironmentCapturingClass.getCurrentEnvironmentDetails();
         LoggerLevelProvider.LOGGER.info(strFeedback);
+    }
+}
+
+/**
+ * Pair bank records with selling receipts and store result-set into CSV file
+ */
+@CommandLine.Command(name = "PairBankRecordsWithSellingReceipts", description = "Pair bank records with selling receipts and store result-set into CSV file")
+class PairBankRecordsWithSellingReceipts implements Runnable {
+
+    final static List<String> listOutResult = new ArrayList<>();
+    /**
+     * String for FileName
+     */
+    @CommandLine.Option(
+            names = {"-flOut", "--outFileName"},
+            description = "File Name to store result-set",
+            arity = "1",
+            required = true)
+    private String strFileNameOut;
+
+    private static void buildRecordString(final String strDateIso8601, final LocalDateTime dtBank,
+                                          final Properties crtRowBank, final int intBankKeyCounter, final int inBankOverallRow,
+                                          final Properties crtRowCashRegister, final int intCashRegisterCounter, final int inCashRegisterRow,
+                                          final String strScenario) {
+        LocalDateTime dtCashRegister = null;
+        String strTimestampCashRegister = "";
+        String crtRowCashRegisterCheie3BF = "";
+        String crtRowCashRegisterHourComparable = "";
+        String crtRowCashRegisterSerialNumber = "";
+        if (!strScenario.contains("FĂRĂ")) {
+            dtCashRegister = TimingClass.getLocalDateTimeFromStrings(strDateIso8601,
+                    crtRowCashRegister.get("HourComparable").toString());
+            strTimestampCashRegister = dtCashRegister.toString().replace("T", " ");
+            crtRowCashRegisterCheie3BF = crtRowCashRegister.get("Cheie3BF").toString();
+            crtRowCashRegisterHourComparable = crtRowCashRegister.get("HourComparable").toString();
+            crtRowCashRegisterSerialNumber = crtRowCashRegister.get("SerialNumber").toString();
+        }
+        final String strDuration = switch(strScenario) {
+            case "Înregistrare Bancă DUPĂ înregistrare Casă Fiscală" -> TimingClass.convertNanosecondsIntoSomething(
+                    Duration.between(dtCashRegister, dtBank), "TimeClockClassic");
+            case "Înregistrare Bancă ÎNAINTE DE înregistrare Casă Fiscală" -> TimingClass.convertNanosecondsIntoSomething(
+                    Duration.between(dtBank, dtCashRegister), "TimeClockClassic");
+            default -> "";
+        };
+        final String strFeedback = String.join(";", crtRowBank.get("OriginalOrder").toString(),
+                crtRowBank.get("Cheie3").toString() + "==>" + crtRowBank.get("OraComparatie").toString(),
+                crtRowBank.get("Cheie3").toString().substring(crtRowBank.get("Cheie3").toString().length() - 7),
+                crtRowCashRegisterCheie3BF,
+                crtRowCashRegisterHourComparable,
+                crtRowCashRegisterSerialNumber,
+                strScenario,
+                String.valueOf((inBankOverallRow + 1)),
+                TimingClass.getCurrentTimestamp("yyyy-MM-dd HH:mm:ss.SSS"),
+                String.valueOf(intBankKeyCounter),
+                String.valueOf(intCashRegisterCounter),
+                dtBank.toString().replace("T", " "),
+                strTimestampCashRegister,
+                strDuration);
+        LoggerLevelProvider.LOGGER.debug(strFeedback + " ----> " + inCashRegisterRow);
+        listOutResult.add(strFeedback); // new line in CSV
+    }
+
+    @Override
+    public void run() {
+        Properties propMySql = DatabaseSpecificMySql.getConnectionPropertiesForMySQL();
+        final Properties qryProperties = new Properties();
+        final Properties propsBank = new Properties();
+        propsBank.put("strWhich", "Purpose is to get all Bank records");
+        propsBank.put("strKind", "Values");
+        propsBank.put("strQueryToUse", """
+SELECT
+      OriginalOrder
+    , Cheie3
+    , OraComparatie
+    , CAST(OraComparatie AS UNSIGNED) AS OraComparatieInt
+FROM
+    mt.bt
+WHERE
+    Relevant = 'Yes'
+ORDER BY
+      Cheie3
+    , OraComparatie
+LIMIT 31;""");
+        final List<Integer> listRowFound = new ArrayList<>();
+        listOutResult.add(String.join(";", "OrdineBT", "Cheie4BT", "Valoare", "Cheie3BF", "OraBF", "SerialNumber"
+                , "Scenariu", "Nr", "TS", "ContorCheieBT", "ContorCheieBF", "TimestampBT", "TimestampBF", "DifTimp")); // header of CSV
+        try (Connection objConnection = DatabaseSpecificMySql.getMySqlConnection(propMySql, "mt");
+             Statement objStatement = DatabaseConnectivity.createSqlStatement("MySQL", objConnection)) {
+            final List<Properties> listBank = DatabaseResultSettingClass.getResultSetStandardized(objStatement, propsBank, qryProperties);
+            int intBankRecs = listBank.size();
+            int intBankKeyCounter = 0;
+            int intCashRegisterCounter = 0;
+            LoggerLevelProvider.LOGGER.debug(String.format("Dimensiunea listei cu înregistrări de bancă este %s", intBankRecs));
+            String strCheie3memory = "<empty>";
+            String strValue = String.valueOf(0);
+            LocalDateTime dtBank = null;
+            List<Properties> listCashRegister = null;
+            Properties crtRowBank;
+            Properties crtRowCashRegister;
+            final String strQueryCashRegisterTemplate = """
+SELECT
+      Cheie3BF
+    , HourComparable
+    , CAST(HourComparable AS UNSIGNED) AS HourComparableInt
+    , SerialNumber
+FROM
+    mt.bf
+WHERE
+    Cheie3BF = '{Cheie3}'
+ORDER BY
+      HourComparable;""";
+            for(int i = 0; i < intBankRecs; i++) { // cycle through Bank records
+                final int intCrtRow = i + 1;
+                crtRowBank = listBank.get(i);
+                intBankKeyCounter++;
+                LoggerLevelProvider.LOGGER.debug("Bank row " + i + " => " + strCheie3memory + " vs. " + crtRowBank.get("Cheie3").toString());
+                if (!strCheie3memory.equalsIgnoreCase(crtRowBank.get("Cheie3").toString())) {
+                    strCheie3memory = crtRowBank.get("Cheie3").toString();
+                    strValue = strCheie3memory.substring(strCheie3memory.length() - 7);
+                    intBankKeyCounter = 1;
+                    final Properties propsCashRegisterKey = new Properties();
+                    propsCashRegisterKey.put("strWhich", "Purpose is to get relevant Cash Register records");
+                    propsCashRegisterKey.put("strKind", "Values");
+                    final String strQueryCashRegister = strQueryCashRegisterTemplate.replace("{Cheie3}", strCheie3memory);
+                    propsCashRegisterKey.put("strQueryToUse", strQueryCashRegister);
+                    listCashRegister = DatabaseResultSettingClass.getResultSetStandardized(objStatement, propsCashRegisterKey, qryProperties);
+                    intCashRegisterCounter = 0;
+                }
+                final String strDateIso8601 = strCheie3memory.substring(strCheie3memory.indexOf("_") + 1,strCheie3memory.indexOf("_") + 12);
+                dtBank = TimingClass.getLocalDateTimeFromStrings(strDateIso8601,
+                        crtRowBank.get("OraComparatie").toString());
+                if (!listCashRegister.isEmpty()) {
+                    int intCashregisterRecs = listCashRegister.size();
+                    LoggerLevelProvider.LOGGER.debug(String.format("Dimensiunea listei cu înregistrări de casă fiscală este %s", intCashregisterRecs));
+                    for(int j = 0; j < intCashregisterRecs; j++) { // cycle through Cash Register records
+                        crtRowCashRegister = listCashRegister.get(j);
+                        LoggerLevelProvider.LOGGER.debug("Cash Register row " + j + " => " + crtRowBank.get("OraComparatieInt").toString()
+                                + " vs. " + crtRowCashRegister.get("HourComparableInt").toString());
+                        intCashRegisterCounter++;
+                        if (listRowFound.stream().noneMatch(n -> n == intCrtRow)) {
+                            if (Integer.parseInt(crtRowBank.get("OraComparatieInt").toString()) >= Integer.parseInt(crtRowCashRegister.get("HourComparableInt").toString())) {
+                                listRowFound.add((i + 1));
+                                buildRecordString(strDateIso8601, dtBank,
+                                        crtRowBank, intBankKeyCounter, i,
+                                        crtRowCashRegister, intCashRegisterCounter, j,
+                                        "Înregistrare Bancă DUPĂ înregistrare Casă Fiscală");
+                                listCashRegister.remove(j);
+                                intCashRegisterCounter++;
+                                intCashregisterRecs--;
+                            }
+                        }
+                    }
+                    intCashRegisterCounter = intCashRegisterCounter - intCashregisterRecs;
+                    for(int j = 0; j < intCashregisterRecs; j++) { // cycle through Cash Register records
+                        crtRowCashRegister = listCashRegister.get(j);
+                        LoggerLevelProvider.LOGGER.debug("Cash Register row " + j + " => " + crtRowBank.get("OraComparatieInt").toString()
+                                + " vs. " + crtRowCashRegister.get("HourComparableInt").toString());
+                        intCashRegisterCounter++;
+                        if (listRowFound.stream().noneMatch(n -> n == intCrtRow)) {
+                            if (Integer.parseInt(crtRowBank.get("OraComparatieInt").toString()) < Integer.parseInt(crtRowCashRegister.get("HourComparableInt").toString())) {
+                                listRowFound.add((i + 1));
+                                buildRecordString(strDateIso8601, dtBank,
+                                        crtRowBank, intBankKeyCounter, i,
+                                        crtRowCashRegister, intCashRegisterCounter, j,
+                                        "Înregistrare Bancă ÎNAINTE DE înregistrare Casă Fiscală");
+                                listCashRegister.remove(j);
+                                intCashRegisterCounter++;
+                                intCashregisterRecs--;
+                            }
+                        }
+                    }
+                    intCashRegisterCounter = intCashRegisterCounter - intCashregisterRecs;
+                }
+                if (listRowFound.stream().noneMatch(n -> n == intCrtRow)) {
+                    buildRecordString(strDateIso8601, dtBank,
+                            crtRowBank, intBankKeyCounter, i,
+                            null, intCashRegisterCounter, 0,
+                            "Înregistrare Bancă FĂRĂ înregistrare Casă Fiscală");
+                }
+            }
+            FileContentClass.writeListToTextFile(listOutResult, strFileNameOut);
+        } catch(SQLException e){
+            if (LoggerLevelProvider.currentLevel.isLessSpecificThan(Level.WARN)) {
+                final String strFeedback = String.format(JavaJavaLocalization.getMessage("i18nSQLerror"), Arrays.toString(e.getStackTrace()), StackWalker.getInstance()
+                        .walk(frames -> frames.findFirst().map(frame -> frame.getClassName() + "." + frame.getMethodName()).orElse(Common.STR_I18N_UNKN)));
+                LoggerLevelProvider.LOGGER.error(strFeedback);
+            }
+        }
     }
 }
