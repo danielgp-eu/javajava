72856a60c60b410d846a20dd00fa2c5d1907fbf7
Daniel Popiniuc <danielpopiniuc@gmail.com>
2026-01-15 22:00:25 +0200

    code refactoring to reduce import weight from ProjectDependencyResolverClass by introducing a collaborating ProjectModelClass + minor version bump

--- src/main/java/environment/ProjectDependencyResolverClass.java
+++ src/main/java/environment/ProjectDependencyResolverClass.java
@@ -1,38 +1,23 @@
 package environment;
 
-import log.LogExposureClass;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.artifact.Artifact;
-import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.graph.DependencyNode;
 import org.eclipse.aether.repository.LocalRepository;
-import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 import org.eclipse.aether.resolution.DependencyResult;
 import org.eclipse.aether.supplier.RepositorySystemSupplier;
 
-import file.ProjectClass;
 import json.JsoningClass;
-import localization.JavaJavaLocalizationClass;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import log.LogExposureClass;
 
 /**
  * This class demonstrates how to resolve all dependencies (including transitive)
@@ -44,29 +29,6 @@ public final class ProjectDependencyResolverClass {
      * Minimum prefix length
      */
     private static final int MIN_PREFIX_LENGTH = 4;
-
-    /**
-     * Create CollectRequest from Maven Model
-     * @param model Model
-     * @return CollectRequest
-     */
-    private static CollectRequest createCollectRequest(final Model model) {
-        final CollectRequest collectRequest = new CollectRequest();
-        final RemoteRepository central = new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/").build();
-        collectRequest.addRepository(central);
-        final List<org.apache.maven.model.Dependency> lstDeps = model.getDependencies();
-        lstDeps.stream().forEach(modelDep -> {
-            final Artifact artifact = new DefaultArtifact(
-                    modelDep.getGroupId(), 
-                    modelDep.getArtifactId(), 
-                    modelDep.getClassifier(), 
-                    modelDep.getType(), 
-                    modelDep.getVersion()
-                );
-            collectRequest.addDependency(new Dependency(artifact, modelDep.getScope()));
-        });
-        return collectRequest;
-    }
 
     /**
      * Helper to recurse through transitive
@@ -118,8 +80,7 @@ public final class ProjectDependencyResolverClass {
         final DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
         final LocalRepository localRepo = new LocalRepository("target/local-repo");
         session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
-        final Model model = getProjectObjectModelFileIntoModel();
-        final CollectRequest collectRequest = createCollectRequest(model);
+        final CollectRequest collectRequest = ProjectModelClass.getCollectRequestForCurrentProject();
         final DependencyNode root = resolveDependencies(system, session, collectRequest);
         final String strFeedback = getDependenciesIntoString(root);
         system.shutdown();
@@ -153,23 +114,6 @@ public final class ProjectDependencyResolverClass {
     private static String getNodeVersion(final DependencyNode directNode) {
         final Artifact node = getNodeArtifact(directNode);
         return node.getVersion();
-    }
-
-    /**
-     * get POM into Model
-     * @return Model
-     */
-    public static Model getProjectObjectModelFileIntoModel() {
-        final String pomFile = ProjectClass.getCurrentProjectObjectModelFile();
-        final MavenXpp3Reader reader = new MavenXpp3Reader();
-        Model model = null;
-        try(BufferedReader bReader = Files.newBufferedReader(Path.of(pomFile), StandardCharsets.UTF_8)) {
-            model = reader.read(bReader);
-        } catch (IOException | XmlPullParserException ex) {
-            final String strFeedback = String.format(JavaJavaLocalizationClass.getMessage("i18nErrorOnGettingDependencies"), Arrays.toString(ex.getStackTrace()));
-            LogExposureClass.LOGGER.error(strFeedback);
-        }
-        return model;
     }
 
     /**
---
+++ src/main/java/environment/ProjectModelClass.java
@@ -0,0 +1,88 @@
+package environment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.collection.CollectRequest;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
+
+import file.ProjectClass;
+import localization.JavaJavaLocalizationClass;
+import log.LogExposureClass;
+
+/**
+ * Project Model
+ */
+public final class ProjectModelClass {
+
+    /**
+     * Create CollectRequest from Maven Model
+     * @param model Model
+     * @return CollectRequest
+     */
+    private static CollectRequest createCollectRequest(final Model model) {
+        final CollectRequest collectRequest = new CollectRequest();
+        final RemoteRepository central = new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/").build();
+        collectRequest.addRepository(central);
+        final List<org.apache.maven.model.Dependency> lstDeps = model.getDependencies();
+        lstDeps.stream().forEach(modelDep -> {
+            final Artifact artifact = new DefaultArtifact(
+                    modelDep.getGroupId(), 
+                    modelDep.getArtifactId(), 
+                    modelDep.getClassifier(), 
+                    modelDep.getType(), 
+                    modelDep.getVersion()
+                );
+            collectRequest.addDependency(new Dependency(artifact, modelDep.getScope()));
+        });
+        return collectRequest;
+    }
+
+    /**
+     * 
+     * @return CollectRequest
+     */
+    public static CollectRequest getCollectRequestForCurrentProject() {
+        final Model model = getProjectObjectModelFileIntoModel();
+        final CollectRequest collectRequest = createCollectRequest(model);
+        final String strFeedback = "I have created Request for Collection of current project!";
+        LogExposureClass.LOGGER.debug(strFeedback);
+        return collectRequest;
+    }
+
+    /**
+     * get POM into Model
+     * @return Model
+     */
+    public static Model getProjectObjectModelFileIntoModel() {
+        final String pomFile = ProjectClass.getCurrentProjectObjectModelFile();
+        final MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = null;
+        try(BufferedReader bReader = Files.newBufferedReader(Path.of(pomFile), StandardCharsets.UTF_8)) {
+            model = reader.read(bReader);
+        } catch (IOException | XmlPullParserException ex) {
+            final String strFeedback = String.format(JavaJavaLocalizationClass.getMessage("i18nErrorOnGettingDependencies"), Arrays.toString(ex.getStackTrace()));
+            LogExposureClass.LOGGER.error(strFeedback);
+        }
+        return model;
+    }
+
+    /**
+     * Constructor
+     */
+    private ProjectModelClass() {
+        // intentionally left blank
+    }
+
+}
