339d49ec8865d0bf22b3e2681ef4150f45c3d4ec
Daniel Popiniuc <danielpopiniuc@gmail.com>
2026-04-01 00:30:51 +0300

    constant syntax fix

--- src/main/java/javajava/TimingClass.java
+++ src/main/java/javajava/TimingClass.java
@@ -39,7 +39,7 @@ public final class TimingClass {
     /**
      * String constant
      */
-    /* default */ public static final int DAY_MILLISECS = 24 * 60 * 60 * 1000;
+    public static final int DAY_MILLISECS = 24 * 60 * 60 * 1000;
     /**
      * Map with predefined network physical types
      */
