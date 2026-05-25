3be404fb53f077df4ec96943df8c090c65abcecc
Daniel Popiniuc <danielpopiniuc@gmail.com>
2026-05-25 23:18:53 +0300

    minor test tweak

--- src/test/java/javajava/BasicStructuresClassTests.java
+++ src/test/java/javajava/BasicStructuresClassTests.java
@@ -104,28 +104,28 @@ class BasicStructuresClassTests {
 
         @Test
         void testCleanStringAsDatabaseObject() {
-            final String strOriginal = "Original";
+            final String strOriginal = "Original1";
             final String handled = BasicStructuresClass.StringCleaningSubClass.cleanStringAsDatabaseObject(strOriginal + "^");
             assertEquals(strOriginal, handled, String.format(ORIG_NQ_EXPCT, handled, strOriginal));
         }
 
         @Test
         void testCleanStringFromCurlyBraces() {
-            final String strOriginal = "Original";
+            final String strOriginal = "Original2";
             final String handled = BasicStructuresClass.StringCleaningSubClass.cleanStringFromCurlyBraces("{" + strOriginal + "}");
             assertEquals(strOriginal, handled, String.format(ORIG_NQ_EXPCT, handled, strOriginal));
         }
 
         @Test
         void testCleanStringFromUnwantedCharacters() {
-            final String strOriginal = "Original";
+            final String strOriginal = "Original3";
             final String handled = BasicStructuresClass.StringCleaningSubClass.cleanStringFromUnwantedCharacters(strOriginal + "^");
             assertEquals(strOriginal, handled, String.format(ORIG_NQ_EXPCT, handled, strOriginal));
         }
 
         @Test
         void testStripQuotes() {
-            final String strOriginal = "Original";
+            final String strOriginal = "Original4";
             final String handled = BasicStructuresClass.StringCleaningSubClass.stripQuotes("\"" + strOriginal + "\"");
             assertEquals(strOriginal, handled, String.format(ORIG_NQ_EXPCT, handled, strOriginal));
         }
