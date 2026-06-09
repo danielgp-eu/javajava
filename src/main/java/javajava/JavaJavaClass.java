6e8abe980832e5a348a6b4837df0692936802b5f
Daniel Popiniuc <danielpopiniuc@gmail.com>
2026-06-09 23:28:51 +0300

    separating Archive Folders as a separate package as rarely used here

--- src/main/java/javajava/JavaJavaClass.java
+++ src/main/java/javajava/JavaJavaClass.java
@@ -22,7 +22,6 @@ import picocli.CommandLine.Mixin;
     subcommands = {
             AnalyzeColumnsFromCsvFiles.class,
             AnalyzePomFiles.class,
-            ArchiveFolders.class,
             CalculateSunriseAndSunset.class,
             CaptureChecksumsOfFilesFromFoldersIntoCsvFile.class,
             CaptureEnvironmentDetailsIntoJsonFile.class,
@@ -158,89 +157,6 @@ class AnalyzePomFiles implements Runnable {
         super();
     }
 
-}
-
-/**
- * clean files older than a given number of days
- */
-@CommandLine.Command(name = "ArchiveFolders",
-                     description = "Archive sub-folders from a given folder")
-class ArchiveFolders implements Runnable {
-
-    /**
-     * String for FolderName
-     */
-    @CommandLine.Option(
-            names = {"-aExe", "--archivingExecutable"},
-            description = "Archiving executable (including full path, optional)")
-    private String strArchivingExec;
-
-    /**
-     * String for archive name prefix
-     */
-    @CommandLine.Option(
-            names = {"-pwd", "--archivePassword"},
-            description = "Password for archive encryption")
-    private String strArchivePwd;
-
-    /**
-     * String for archive name prefix
-     */
-    @CommandLine.Option(
-            names = {"-ap", "--archivePrefix"},
-            description = "Prefix to apply to archive name")
-    private String strArchivePrefix;
-
-    /**
-     * String for archive name prefix
-     */
-    @CommandLine.Option(
-            names = {"-as", "--archiveSuffix"},
-            description = "Suffix to apply to archive name")
-    private String strArchiveSuffix;
-
-    /**
-     * adds the options defined in 
-     * CommonInteractiveClass.FolderNameOptionMixinClass to this command
-     */
-    @Mixin
-    private final CommonInteractiveClass.FolderNameOptionMixinClass optFolderNames = new CommonInteractiveClass.FolderNameOptionMixinClass();
-
-    /**
-     * adds the options defined in 
-     * CommonInteractiveClass.FolderDestinationOptionMixinClass to this command
-     */
-    @Mixin
-    private final CommonInteractiveClass.FolderDestinationOptionMixinClass optFolderDest = new CommonInteractiveClass.FolderDestinationOptionMixinClass();
-
-    @Override
-    public void run() {
-        final Properties propFolder = new Properties();
-        if (strArchivingExec != null) {
-            ArchivingClass.setArchivingExecutable(strArchivingExec);
-        }
-        ArchivingClass.setArchivePrefix(strArchivePrefix);
-        ArchivingClass.setArchiveSuffix(strArchiveSuffix);
-        if (strArchivePwd != null) {
-            ArchivingClass.setArchivePwd(strArchivePwd);
-        }
-        final String[] inFolders = optFolderNames.getFolderNames();
-        for (final String strFolder : inFolders) {
-            propFolder.clear();
-            final Properties folderProps = FileOperationsClass.StatisticsSubClass.getFolderStatisticsRecursive(strFolder, propFolder);
-            ArchivingClass.setArchivingDir(strFolder);
-            ArchivingClass.setArchiveNameWithinDestinationFolder(optFolderDest.getFolderDestination());
-            ArchivingClass.archiveFolderAs7z();
-            ArchivingClass.exposeArchivedStatistics(folderProps);
-        }
-    }
-
-    /**
-     * Constructor
-     */
-    protected ArchiveFolders() {
-        super();
-    }
 }
 
 /**
